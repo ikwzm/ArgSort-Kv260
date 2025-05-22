c8615a860daecd11d41f0bee51ff1ab9c3475d3d
Ichiro Kawazome <ichiro_k@ca2.so-net.ne.jp>
2025-05-22 10:09:00 +0900

    [change] templete files for build with both vivado 2021.2 and 2023.1

--- argsort_template/common.mk
+++ argsort_template/common.mk
@@ -8,23 +8,18 @@ BIN_FILE      ?= $(BIN_FILE_PATH)/argsort_$(MRG_WAYS)_$(MRG_WORDS)_$(STM_FEEDBAC
 BIT_FILE      ?= project.runs/impl_1/design_1_wrapper.bit
 TEMPLATE_PATH ?= $(dir $(lastword $(MAKEFILE_LIST)))
 VIVADO_FILES  ?= create_project.tcl implementation.tcl add_fan_enable.tcl design_1_pin.xdc design_1.bif 
-DESIGN_FILE   ?= design_1_bd_2021.2.tcl
+DESIGN_FILES  ?= design_1_bd_2021.2.tcl design_1_bd_2023.1.tcl
 
 all    : $(BIN_FILE)
 
-scripts: $(DESIGN_FILE) $(VIVADO_FILES)
+scripts: check-required-vars $(DESIGN_FILES) $(VIVADO_FILES)
 
 project: project.xpr
 
 binary : $(BIN_FILE)
 
 clean  :
-	$(REMOVE) $(VIVADO_FILES) $(DESIGN_FILE) project.xpr
-
-define GET_VIVADO_FILES
-$(1) : $(addprefix $(TEMPLATE_PATH)/,$(1))
-	$(COPY) $(addprefix $(TEMPLATE_PATH)/,$(1)) $(1)
-endef
+	$(REMOVE) $(VIVADO_FILES) $(DESIGN_FILES) project.xpr
 
 REQUIRED_VARS := MRG_WAYS MRG_WORDS STM_FEEDBACK CLK_FREQ
 check-required-vars:
@@ -32,16 +27,25 @@ check-required-vars:
 		$(if $(value $(var)),,    \
 			$(error Required variable "$(var)" is not defined)))
 
-$(DESIGN_FILE) : $(addprefix $(TEMPLATE_PATH)/,$(DESIGN_FILE)) check-required-vars
+define GET_DESIGN_FILE
+$(1) : $(addprefix $(TEMPLATE_PATH)/,$(1))
 	$(SED) -e 's/\(.*CONFIG.MRG_WAYS\).*/\1 {$(MRG_WAYS)} \\/'                   \
                -e 's/\(.*CONFIG.MRG_WORDS\).*/\1 {$(MRG_WORDS)} \\/'                 \
                -e 's/\(.*CONFIG.STM_FEEDBACK\).*/\1 {$(STM_FEEDBACK)} \\/'           \
                -e 's/\(.*CONFIG.CLKOUT1_REQUESTED_OUT_FREQ\).*/\1 {$(CLK_FREQ)} \\/' \
-               $< > $@
+               $(addprefix $(TEMPLATE_PATH)/,$(1)) > $(1)
+endef
 
-$(foreach FILE, $(VIVADO_FILES), $(eval $(call GET_VIVADO_FILES, $(FILE))))
+$(foreach FILE, $(DESIGN_FILES), $(eval $(call GET_DESIGN_FILE,$(FILE))))
 
-project.xpr : $(VIVADO_FILES) $(DESIGN_FILE)
+define GET_VIVADO_FILE
+$(1) : $(addprefix $(TEMPLATE_PATH)/,$(1))
+	$(COPY) $(addprefix $(TEMPLATE_PATH)/,$(1)) $(1)
+endef
+
+$(foreach FILE, $(VIVADO_FILES), $(eval $(call GET_VIVADO_FILE,$(FILE))))
+
+project.xpr : $(VIVADO_FILES) $(DESIGN_FILES)
 	$(VIVADO) -mode batch -source create_project.tcl
 
 $(BIT_FILE) : project.xpr
